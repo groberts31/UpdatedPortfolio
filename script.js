207c87265e595edf40bbd56ebc4903d9c86a8e0e
Greg Roberts <groberts3104@gmail.com>
2019-12-11 20:27:48 -0500

    added script.js and added script to console log info entered into forms on portfolio.html

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#submit").click(function () {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
+        var contact = $("#contact").val();
+
+        if (name == '' || email == '' || contact == '') {
+            alert("Please Fill Required Fields");
+        } else {
+            console.log(name);
+            console.log(email);
+            console.log(message);
+            console.log(contact);
+        }
+    });
+})
